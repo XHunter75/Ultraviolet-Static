f8a04c8f8dac340eafc8b355c8d665a3760567e1
Ultra <dprabhav75@gmail.com>
2026-03-22 14:06:43 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,11 @@
 self.__uv$config = {
-  // Add your repository name to all these paths
-  prefix: "/Ultraviolet-Static/uv/service/",
-  bare: "https://uv.student-portal.xyz", 
+  prefix: "/Ultraviolet-Static/uv/service/", // Added folder name here
+  bare: "https://bare.benroberts.dev", 
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/Ultraviolet-Static/uv/uv.handler.js",
-  client: "/Ultraviolet-Static/uv/uv.client.js",
-  bundle: "/Ultraviolet-Static/uv/uv.bundle.js",
-  config: "/Ultraviolet-Static/uv/uv.config.js",
-  sw: "/Ultraviolet-Static/uv/uv.sw.js",
+  handler: "/Ultraviolet-Static/uv/uv.handler.js", // Added folder name here
+  client: "/Ultraviolet-Static/uv/uv.client.js",   // Added folder name here
+  bundle: "/Ultraviolet-Static/uv/uv.bundle.js",   // Added folder name here
+  config: "/Ultraviolet-Static/uv/uv.config.js",   // Added folder name here
+  sw: "/Ultraviolet-Static/uv/uv.sw.js",           // Added folder name here
 };
